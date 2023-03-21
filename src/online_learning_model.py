f7b6f86d8044624884c159765cdb770f074c0cef
integration-test integration-test <test-notifs+integration-test@dominodatalab.com>
2023-03-21 14:00:40 -0700

    Pushed from Domino: https://aws-iam7653.cs.domino.tech/workspace/integration-test/streaming-online-learning-icde?executionId=641a08449bc2112a585a28ba

--- src/online_learning_model.py
+++ src/online_learning_model.py
@@ -84,7 +84,7 @@ def consume_features(group_id:str):
     print(f'\nNow subscribing to features topic {feature_topic}')
     features_consumer.subscribe([feature_topic])
     print(features_consumer_conf)
-    
+    return
     producer_conf = {'bootstrap.servers': KAFKA_BOOTSTRAP_SERVERS,
                      'sasl.username': KAFKA_USER_NAME,
                      'sasl.password': KAFKA_PASSWORD,
